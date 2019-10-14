4968012e422c4f9e80bbecb2c5c841ed4ab1a250
RomichG <hello@romich-g.com>
2019-10-14 14:55:34 -0400

    bringing back the CSRF check

--- app.py
+++ app.py
@@ -148,9 +148,9 @@ def spell_check():
         token = request.form.get('token')
 
         # CSRF Check
-#        our_token = session.get('token')
-#        if token != our_token:
-#            return render_template('404.html')
+        our_token = session.get('token')
+        if token != our_token:
+            return render_template('404.html')
         session.pop('token', None)
 
         # TODO: validate input
